00b40893b9d869436ee05d0d46f5cac211294a40
Seonhyuk <gomduridkstjsgur@gmail.com>
2023-01-01 23:46:39 +0900

    add plugin practice

--- webpack_study/webpack.config.js
+++ webpack_study/webpack.config.js
@@ -1,7 +1,12 @@
 const path = require("path");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: {
     main: "./src/app.js",
   },
@@ -13,7 +18,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [process.env.NODE_ENV === "production" ? MiniCssExtractPlugin.loader : "style-loader", "css-loader"],
       },
       {
         test: /\.png$/,
@@ -21,4 +26,29 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+        Build Date: ${new Date().toLocaleString()}
+        Commit Version: ${childProcess.execSync("git rev-parse --short HEAD")}
+        Author: ${childProcess.execSync("git config user.name")}
+      `,
+    }),
+    new webpack.DefinePlugin({
+      TWO: "1+1",
+      "api.domain": JSON.stringify("https://"),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "(배포용)",
+      },
+      minify: {
+        collapseWhitespace: false,
+        removeComments: true,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production" ? [new MiniCssExtractPlugin({ filename: "[name].css" })] : []),
+  ],
 };
