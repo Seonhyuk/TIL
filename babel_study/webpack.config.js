760223b3877e557d515003beacfdafe47bd72f61
Seonhyuk <gomduridkstjsgur@gmail.com>
2023-01-03 19:26:08 +0900

    add babel-study

---
+++ babel_study/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [process.env.NODE_ENV === "production" ? MiniCssExtractPlugin.loader : "style-loader", "css-loader"],
+      },
+      {
+        test: /\.png$/,
+        type: "asset",
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+        Build Date: ${new Date().toLocaleString()}
+        Commit Version: ${childProcess.execSync("git rev-parse --short HEAD")}
+        Author: ${childProcess.execSync("git config user.name")}
+      `,
+    }),
+    new webpack.DefinePlugin({
+      TWO: "1+1",
+      "api.domain": JSON.stringify("https://"),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "(배포용)",
+      },
+      minify: {
+        collapseWhitespace: false,
+        removeComments: true,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production" ? [new MiniCssExtractPlugin({ filename: "[name].css" })] : []),
+  ],
+};
